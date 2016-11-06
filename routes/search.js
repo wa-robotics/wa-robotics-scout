826f6afc2eb1ff7f3b9370cbc7921b560b8b2c3f
Evan Strat <evan10s@users.noreply.github.com>
2016-11-06 00:19:42 -0400

    Start updating to use EJS and express
    Team search and index pages now work (now GET param support yet)

--- routes/search.js
+++ routes/search.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: 'WARS: Dashboard'
+    res.render('search', {
+        title: 'WARS: Search teams'
     });
 });
 module.exports = router;
