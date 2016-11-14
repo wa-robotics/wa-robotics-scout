7d95702c6de28a7d223460b2c3aba90b8c74cec0
Evan Strat <evan10s@users.noreply.github.com>
2016-11-14 18:19:06 -0500

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+function getTeamMatchesVexDb(res, sku, teamNum) {
+    teamNum = teamNum.toString();
+    if (!/^[0-9]{1,5}[a-zA-Z]?$/.test(teamNum)) {
+        res.send(JSON.stringify({
+            error: 1
+            , message: "Invalid team number entered"
+        }));
+    }
+    else {
+        request("https://api.vexdb.io/v1/get_matches?sku=" + sku + "&team=" + teamNum, function (error, response, body) {
+            var raw = body;
+            var parsed = JSON.parse(raw);
+            var results = {
+                status: 1
+                , results: parsed.result
+            }
+            console.log(results);
+            res.send(JSON.stringify(results));
+        })
+    }
+}
+
+function getUnscoredMatches(res, sku, num) {
+    request("https://api.vexdb.io/v1/get_matches?scored=0&limit_number=" + num + "&sku=" + sku, (error, response, body) => {
+        var raw = body;
+        var parsed = JSON.parse(raw);
+        var results = {
+            status: 1
+            , results: parsed.result
+        }
+        console.log(results);
+        res.send(JSON.stringify(results));
+    });
+}
+router.get('/:sku/:team', function (req, res, next) {
+    res.set('Content-Type', 'application/json');
+    getTeamMatchesVexDb(res, req.params.sku, req.params.team);
+});
+router.get('/:sku/unscored/:num', (req, res, next) => {
+    res.set('Content-Type', 'application/json');
+    getUnscoredMatches(res, req.params.sku, parseInt(req.params.num));
+});
+module.exports = router;
