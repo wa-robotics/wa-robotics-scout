2f54bc7e8c274014de7cacacd44a98f3a4974580
Evan Strat <evan10s@users.noreply.github.com>
2016-11-14 18:10:44 -0500

    Add new routes for scouting and api paths

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var search = require('./routes/search');
+var api = require('./routes/api');
+var scout = require('./routes/scout');
+var request = require('request');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/search', search);
+app.use('/api', api);
+app.use('/scout', scout);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
