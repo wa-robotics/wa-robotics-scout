ac9a00dcf77dbe3c2754f6bca713be75a835aa61
Evan Strat <evan10s@users.noreply.github.com>
2016-11-23 12:43:41 -0500

    initial work on unit tests with Mocha

--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,25 @@
 var assert = require('assert');
+var request = require('supertest');
+
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
-            assert.equal(0, [1,2,3].indexOf(4));
+            assert.equal(-1, [1,2,3].indexOf(4));
+        });
+    });
+});
+
+let url = request('http://localhost:3000/api');
+describe ('API',function() {
+    describe("Unscored matches", function(done) {
+        it('should return something no matter what', function() {
+            url.get('/RE-VRC-12-1234/unscored/3')
+                .expect(200)
+                .end(function(err, response){
+                    assert.ok(!err);
+                    assert.ok(typeof response.body.result === 'number');
+                    return done();
+                });
         });
     });
 });
