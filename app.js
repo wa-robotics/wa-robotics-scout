99a7b0d89cc870727838917feae4d565281a1566
Evan Strat <evan10s@users.noreply.github.com>
2016-11-26 21:47:44 -0500

    Require HTTP basic auth to access staging app

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require("express");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-var index = require("./routes/index");
-var search = require("./routes/search");
-var api = require("./routes/api");
-var scout = require("./routes/scout");
-var authFlow = require("./routes/auth");
-
-var request = require("request");
-var app = express();
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const index = require("./routes/index");
+const search = require("./routes/search");
+const api = require("./routes/api");
+const scout = require("./routes/scout");
+const authFlow = require("./routes/auth");
+const utils = require("./utils");
+let request = require("request");
+const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -24,6 +24,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+//auth for staging instance on heroku
+if (process.env.DEPLOY_TYPE === "STAGING") {
+    app.use("/*", utils.basicAuth(1, 2));
+}
+
+
 app.use("/", index);
 app.use("/search", search);
 app.use("/api", api);
@@ -31,7 +38,7 @@ app.use("/scout", scout);
 app.use("/auth",authFlow);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error("Not Found");
+    const err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
@@ -42,6 +49,6 @@ app.use(function (err, req, res, next) {
     res.locals.error = req.app.get("env") === "development" ? err : {};
     // render the error page
     res.status(err.status || 500);
-    res.render("error");
+    res.render("error", {showMenu: true });
 });
 module.exports = app;
