e73cca5a7737e67f4921ebe3b1bf599abf216719
Evan Strat <evan10s@users.noreply.github.com>
2016-12-06 08:22:58 -0500

    Add HTML for match info page
    Signed-off-by: Evan Strat <evan10s@users.noreply.github.com>

--- routes/scout.js
+++ routes/scout.js
@@ -3,6 +3,19 @@ var router = express.Router();
 
 //show an error if no sku is provided
 //TODO: this should go to a page where the user can choose a tournament to use for scouting
+
+router.get('/matchinfo',function (req, res, next) {
+    //let sanitizedSku = req.params.sku.toUpperCase().trim();
+    /*if (!/RE-VRC-[0-9]{2}-[0-9]{4}/gi.test(sanitizedSku)) {
+     next();
+     }*/
+    res.render('matchinfo', {
+        title: 'WARS: Scout matches',
+        tournament: req.params.tournament,
+        org: req.params.org
+    });
+});
+
 router.get('/', function (req, res, next) {
     /*const err = new Error("Bad Request");
     res.locals.message = err.message;
